b1a9a1ededc1322167bc473266bebcad21d344d4
meraeugene <andrew.villalon@1.ustp.edu.ph>
2025-11-18 21:04:25 +0800

    Implement password hashing and verification functions in security module

--- app/utils/security.py
+++ app/utils/security.py
@@ -3,7 +3,10 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    max_len = 72
+    password_trunc = password[:max_len]
+    print("Password value:", repr(password_trunc), "Length (chars):", len(password_trunc))
+    return pwd_context.hash(password_trunc)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
